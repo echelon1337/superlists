169e5267fd5ecf5e5191b7fc2e9507510fe1efc8
echelon1337 <echelon1337@protonmail.com>
2017-02-28 22:14:39 +0100

    Basic view now returns minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-    pass
+def home_page(req):
+    return HttpResponse('<html><title>Listy rzeczy do zrobienia</title></html>')
+
